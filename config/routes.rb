4d03a1660e71d8d3cde56765e5b58820119ca4ca
Kevin Molina <kevinrumolina26@gmail.com>
2017-09-29 21:50:50 -0500

    bootstrap modifications

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     passwords: "users/passwords"
   }
+  get "/users/index" => "users#index"
   root to: "welcome#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
